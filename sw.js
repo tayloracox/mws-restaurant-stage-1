687257d3a79d2b20635382e1c099973f55a2fc01
Taylor Cox <taylorashtoncox@gmail.com>
2018-06-11 09:12:44 -0400

    feat: Addresses Udacity reviewer feedback - Adds images to cache list - Adds application role to map on index and restaurant pages

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-let cacheName = 'v2';
+let cacheName = 'v3';
 let cacheFiles = [
   './',
   './index.html',
   './restaurant.html',
   './css/styles.css',
-  './js/main.js'
+  './js/main.js',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg'
 ]
 
 // Installs Service Worker
@@ -36,6 +46,11 @@ self.addEventListener('activate', function(e) {
 })
 
 // Fetches Service Worker
-self.addEventListener('fetch', function(e) {
-  console.log("[ServiceWorker] Fetching", e.request.url);
+self.addEventListener('fetch', event => {
+  console.log("[ServiceWorker] Fetching"
+  event.respondWith(caches.match(event.request).then(response => {
+    console.log('used cashes')
+    return response || fetch(event.request);
+  })
+  )
 })
