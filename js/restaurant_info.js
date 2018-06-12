485e3d3a19c856fabf9a3c59c32ab3f0263fd1bf
Taylor Cox <taylorashtoncox@gmail.com>
2018-06-12 09:03:11 -0400

    feat: Updates with feedback from review - Adds aria-labels to reviews and breadcrumbs - Moves map role to div rather than section - Moves breadcrumbs to nav - Moves h1 out of nav and into header - Updates styling to keep consistency with original layout

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -149,6 +149,8 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute("aria-current", "page")
+  console.log(li);
   breadcrumb.appendChild(li);
 }
 
